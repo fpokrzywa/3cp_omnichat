6724fce1a1ef50365ab873ec617cc827f18b606f
fpokrzywa <freddie@3cpublish.com>
2025-07-24 19:04:05 -0400

    Updated PromptCatalogPage.tsx

--- src/components/PromptCatalogPage.tsx
+++ src/components/PromptCatalogPage.tsx
@@ -56,7 +56,7 @@ const PromptCatalogPage: React.FC<PromptCatalogPageProps> = ({ onPromptSelect })
         tags: prompt.tags,
         user: (prompt as any).user,
         system: (prompt as any).system,
-        owner: prompt.owner
+        owner: (prompt as any).owner
       }));
       setPrompts(convertedPrompts);
       
