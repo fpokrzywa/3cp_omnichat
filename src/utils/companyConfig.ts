f4c37f9168de55af855cfa76a387ea167aab7d51
fpokrzywa <freddie@3cpublish.com>
2025-07-25 08:23:31 -0400

    Replace bot names with config file

--- src/utils/companyConfig.ts
+++ src/utils/companyConfig.ts
@@ -2,9 +2,15 @@ import companyConfig from '../../company_config.json';
 
 interface CompanyConfig {
   COMPANY_NAME: string;
+  COMPANY_BOT_NAME: string;
 }
 
 export const getCompanyName = (): string => {
   const config = companyConfig[0] as CompanyConfig;
   return config.COMPANY_NAME || 'Advance Solutions';
+};
+
+export const getCompanyBotName = (): string => {
+  const config = companyConfig[0] as CompanyConfig;
+  return config.COMPANY_BOT_NAME || 'OmniChat';
 };
